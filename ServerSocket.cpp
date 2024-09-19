f1b66c2fdf10fa813d1a35a0ac73979236a2af22
Ahmed-Mahmoud-M <ahmedmahmoud842003@gmail.com>
2024-09-19 18:35:37 +0300

    .\ServerSocket.cpp is ready

--- ServerSocket.cpp
+++ ServerSocket.cpp
@@ -1,55 +1,52 @@
-//#include "socket.h"
 #include <iostream>
 #include <string>
 
-
-
-class ServerSocket 
-{
-public:
-  
-    
-    int bindsocket();
-    int listnForConnections();
-    int acceptConnection();
-    void displayServerInfo();
-
-
-};
-
-ServerSocket::ServerSocket(){
-    
-    std::cout<< "Enter ServerSocket ip address : ";
-
-}
-
-
-
-
-
-
-
-
-void ServerSocket::displayServerInfo() {
-        
-
-}
-
-
-
-
+// Define struct socketAddress first
 struct socketAddress {
     std::string ipaddress;
     int port;
 
-    
     void displayInfo() {
         std::cout << "IP Address: " << ipaddress << ", Port: " << port << std::endl;
     }
 };
 
+class ServerSocket {
+public:
+    socketAddress sockaddr;
+    ServerSocket(); // Default constructor
+    ServerSocket(const socketAddress& addr); // Parameterized constructor
+    ~ServerSocket();
 
+    int bindsocket();
+    int listnForConnections();
+    int acceptConnection();
+    void displayServerInfo();
+};
 
+// Implement the constructors
+ServerSocket::ServerSocket() : sockaddr({"", 0}) {} // Initialize sockaddr with default values
 
+ServerSocket::ServerSocket(const socketAddress& addr) : sockaddr(addr) {} // Correct initialization
 
+ServerSocket::~ServerSocket() {} // Add destructor implementation if needed
 
+void ServerSocket::displayServerInfo() {
+    sockaddr.displayInfo();
+}
+
+// Implement other methods (bindsocket, listnForConnections, acceptConnection)
+int ServerSocket::bindsocket() {
+    // Implementation of bindsocket
+    return 0;
+}
+
+int ServerSocket::listnForConnections() {
+    // Implementation of listnForConnections
+    return 0;
+}
+
+int ServerSocket::acceptConnection() {
+    // Implementation of acceptConnection
+    return 0;
+}
