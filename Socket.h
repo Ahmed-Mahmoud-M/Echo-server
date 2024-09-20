32eab8d21cd3ce164d9f76ff557f55054bcd6dbe
Ahmed-Mahmoud-M <ahmedmahmoud842003@gmail.com>
2024-09-20 19:36:06 +0300

    making wsastartup() and createsocket()

--- Socket.h
+++ Socket.h
@@ -7,13 +7,12 @@
     serversocket and clientsocket will 
     inhernt from it  (these methods are common between the two)*/
 
-class socket
+class Socket
 {
 
 
 public:
    
-//    virtual int createSocket() = 0;
 //    virtual int closeSocket() = 0;
 //    virtual int send(int values)=0;
 //    virtual int recieve()=0;
@@ -21,22 +20,39 @@ public:
 
    virtual int wsaStartup(){
         // Initialize WSA variables
-    WSADATA wsaData;
-    int wsaerr;
-    WORD wVersionRequested = MAKEWORD(2, 2);
-    wsaerr = WSAStartup(wVersionRequested, &wsaData);
 
-    // Check for initialization success
-    if (wsaerr != 0) {
-        std::cout << "The Winsock dll not found!" << std::endl;
-        return 0;
-    } else {
-        std::cout << "The Winsock dll found" << std::endl;
-        std::cout << "The status: " << wsaData.szSystemStatus << std::endl;
-    }
+        WORD wversionRequested = MAKEWORD(2,2);
+        WSADATA wsaData;
+        int wsaerr =0;
 
-    return 1;
+        wsaerr = WSAStartup(wversionRequested,&wsaData);
+        if (wsaerr!=0) {
+            std::cout << "WSA dll not found" << std::endl;
+            return 1;
+        }else{
+            std::cout << "WSA dll initialized" << std::endl;
+            std::cout << "status :" <<wsaData.szSystemStatus << std::endl;
+        }
+
+        return 0;   
    }
+
+      virtual SOCKET createSocket() {
+        SOCKET newsocket = INVALID_SOCKET;
+
+        newsocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+        if(newsocket == INVALID_SOCKET) {
+            std::cout <<"failed to create socket" << std::endl;
+            WSACleanup();
+        }else{
+            std::cout << "socket created createsocket() " << std::endl;
+        }
+
+
+        return newsocket;
+
+      }
+
 
 };
 
